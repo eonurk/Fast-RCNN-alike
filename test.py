ad7a58a6d6d37e16c734e4cc3172d9767230e952
Onur <onur.karakaslar@bilkent.edu.tr>
2018-12-25 11:29:44 +0300

    added localization accuracies

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from sklearn.metrics import confusion_matrix
 from preprocessing import preprocessing
+from collections import namedtuple
 from train import extractFeatures
 from pathlib import Path
 from PIL import Image
@@ -17,6 +18,14 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 
+# https://stackoverflow.com/questions/27152904/calculate-overlapped-area-between-two-rectangles
+def area(a, b):  # returns None if rectangles don't intersect
+    dx = min(a.xmax, b.xmax) - max(a.xmin, b.xmin)
+    dy = min(a.ymax, b.ymax) - max(a.ymin, b.ymin)
+    if (dx>=0) and (dy>=0):
+        return dx*dy
+
+
 modelPath = Path("model.joblib")
 model = []
 if not modelPath.is_file():
@@ -30,9 +39,11 @@ trueLabels = df[0].values
 trueBoxes = df.drop(0,axis=1).values
 predictedLabels = []
 predictedBoxes = []
+localization = []
+Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
 
 pbar = progressbar.ProgressBar()
-testImgCount = 99
+testImgCount = 100
 for i in range(testImgCount):
   image = Image.open("test/images/"+str(i)+".JPEG").convert('RGB') # load an image
   image = np.asarray(image) # convert to a numpy array
@@ -61,15 +72,27 @@ for i in range(testImgCount):
     currentProb = np.amax(model.predict_proba(boxFeatures))
     if maxProb < currentProb:
       maxProb = currentProb
-      chosenRegion = r['rect']
+      cr = r['rect'] # chosen region
       chosenClass = model.predict(boxFeatures)
-  pbar.finish()
   predictedLabels.append(chosenClass)
-  predictedBoxes.append(chosenRegion)
+  predictedBoxes.append(cr)
 
-pdb.set_trace()
+  pbar.finish()
+
+  if (chosenClass == trueLabels[i]):
+    r_pred = Rectangle(cr[0], cr[1], cr[0]+cr[2], cr[1]+cr[3])
+    r_true = Rectangle(trueBoxes[i][0], trueBoxes[i][1], trueBoxes[i][0]+trueBoxes[i][2], trueBoxes[i][1]+trueBoxes[i][3])
+    intersection = area(r_true,r_pred)
+    union = (cr[2]*cr[3] + trueBoxes[i][2]*trueBoxes[i][3]) - intersection
+    localization.append(intersection/union)
+  else :
+    localization.append(-1) # not a correctly classified image
+  # pdb.set_trace() # dont forget to comment to take run!
+
+print(localization)
 print(confusion_matrix(trueLabels[0:testImgCount], predictedLabels))
 
+#### TO SEE THE BOUNDING BOXES UNCOMMENT THIS PART ####
 #   # draw rectangles on the original image
 #   fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
 #   ax.imshow(image)
